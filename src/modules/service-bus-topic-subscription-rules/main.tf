5b49098a6c12d62ed031c160450f5242cd03549f
Carl Burnett <carlburnett07@gmail.com>
2020-05-25 16:45:31 +0100

    Remove Unused Modules

--- src/modules/service-bus-topic-subscription-rules/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_servicebus_subscription_rule" "sb_topic_sub_rule_sql" {
-  name                = "${var.sb_name}.${var.topic_name}.${var.msg_name}.${var.msg_version}"
-  resource_group_name = var.rg_name
-  namespace_name      = var.sb_name
-  topic_name          = var.topic_name
-  subscription_name   = var.sub_name
-  filter_type         = "SqlFilter"
-  sql_filter          = var.sql_filter
-}
--- src/modules/service-bus-topic-subscriptions/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "sb_topic_subscription_name" {
-    value = azurerm_servicebus_subscription.sb_topic_subscription.name
-}
