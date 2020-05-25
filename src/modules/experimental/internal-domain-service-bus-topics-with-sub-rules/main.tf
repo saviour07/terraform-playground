dc07a2309c29c734b4ba10ec59951e45204b85cd
Carl Burnett <carlburnett07@gmail.com>
2020-05-25 16:52:38 +0100

    Move Experimental Modules Into Main Module Folder
    Moved servicebus topics module out of experimental dir
    Moved servicebus topic subscriptions module out of experimental dir
    Moved servicebus topic subscription rules module out of experimental dir
    Updated references to modules in internal domain module

--- src/modules/experimental/internal-domain-service-bus-topics-with-sub-rules/main.tf
+++ src/modules/experimental/internal-domain-service-bus-topics-with-sub-rules/main.tf
@@ -73,7 +73,7 @@ locals {
 }
 
 module "topics_test" {
-    source = "../service-bus-topics"
+    source = "../../service-bus-topics"
 
     rg_name = var.rg_name
     sb_name = var.sb_name
@@ -81,7 +81,7 @@ module "topics_test" {
 }
 
 module "subs_test" {
-    source = "../service-bus-topic-subscriptions"
+    source = "../../service-bus-topic-subscriptions"
 
     rg_name = var.rg_name
     sb_name = var.sb_name
@@ -89,7 +89,7 @@ module "subs_test" {
 }
 
 module "rules_test" {
-    source = "../service-bus-topic-subscription-rules"
+    source = "../../service-bus-topic-subscription-rules"
 
     rg_name = var.rg_name
     sb_name = var.sb_name
